abe4fb4064a35d92c642f8f06bc86813aa75b098
Jordan Thompson <jordanslost@gmail.com>
2023-05-25 01:44:55 -0700

    Add "mix" type to WAS_Image_Power_Noise

--- WAS_Node_Suite.py
+++ WAS_Node_Suite.py
@@ -3720,7 +3720,7 @@ class WAS_Image_Power_Noise:
                 "height": ("INT", {"default": 512, "max": 4096, "min": 64, "step": 1}),
                 "frequency": ("FLOAT", {"default": 0.5, "max": 10.0, "min": -10.0, "step": 0.01}),
                 "attenuation": ("FLOAT", {"default": 0.5, "max": 10.0, "min": -10.0, "step": 0.01}),
-                "noise_type": (["grey", "white", "pink", "blue", "green"],),
+                "noise_type": (["grey", "white", "pink", "blue", "green", "mix"],),
                 "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),                
             },
         }
@@ -3737,24 +3737,7 @@ class WAS_Image_Power_Noise:
 
         return (pil2tensor(noise_image), )      
 
-
     def generate_power_noise(self, width, height, frequency=None, attenuation=None, noise_type="white", seed=None):
-        '''
-            wdith: Noise Width in pixels
-            height: Noise Height in pixels
-            frequency: The frequency parameter controls the distribution of the noise across different frequencies. 
-                In the context of Fourier analysis, higher frequencies represent fine details or high-frequency components, 
-                while lower frequencies represent coarse details or low-frequency components. Adjusting the frequency 
-                parameter can result in different textures and levels of detail in the generated noise. The specific range 
-                and meaning of the frequency parameter may vary depending on the noise type.
-            attenuation: The attenuation parameter determines the strength or intensity of the noise. It controls how much 
-                the noise values deviate from the mean or central value. Higher values of attenuation lead to more significant 
-                variations and a stronger presence of noise, while lower values result in a smoother and less noticeable noise. 
-                The specific range and interpretation of the attenuation parameter may vary depending on the noise type.
-            noise_type: The tyoe of Power-Law noise to generate (white, grey, pink, green, blue)
-            seed: Seed to generate noise with
-        '''
-        
         def white_noise(width, height):
             noise = np.random.random((height, width))
             return noise
@@ -3771,7 +3754,7 @@ class WAS_Image_Power_Noise:
             power[0, 0] = 1
             noise = np.fft.ifft2(np.fft.fft2(noise) * power)
             noise *= scale / noise.std()
-            return noise
+            return np.real(noise)
 
         def green_noise(width, height, frequency, attenuation):
             noise = grey_noise(width, height, attenuation)
@@ -3781,7 +3764,7 @@ class WAS_Image_Power_Noise:
             power[0, 0] = 1
             noise = np.fft.ifft2(np.fft.fft2(noise) / np.sqrt(power))
             noise *= scale / noise.std()
-            return noise
+            return np.real(noise)
 
         def blue_noise(width, height, frequency, attenuation):
             noise = grey_noise(width, height, attenuation)
@@ -3791,7 +3774,43 @@ class WAS_Image_Power_Noise:
             power[0, 0] = 1
             noise = np.fft.ifft2(np.fft.fft2(noise) / power)
             noise *= scale / noise.std()
-            return noise
+            return np.real(noise)
+
+        def blue_noise_mask(width, height, frequency, attenuation, seed, num_masks=3):
+            masks = []
+            for i in range(num_masks):
+                mask_seed = seed + i 
+                np.random.seed(mask_seed)
+                mask = blue_noise(width, height, frequency, attenuation)
+                masks.append(mask)
+            return masks
+            
+        def blend_noise(width, height, masks, noise_types, attenuations):
+            blended_image = Image.new("L", (width, height), color=0)
+            i = 0
+            for mask, noise_type, attenuation in zip(masks, noise_types, attenuations):
+                mask = Image.fromarray((255 * (mask - np.min(mask)) / (np.max(mask) - np.min(mask))).astype(np.uint8).real)
+                if noise_type == "white":
+                    noise = white_noise(width, height)
+                    noise = Image.fromarray((255 * (noise - np.min(noise)) / (np.max(noise) - np.min(noise))).astype(np.uint8).real)
+                elif noise_type == "grey":
+                    noise = grey_noise(width, height, attenuation)
+                    noise = Image.fromarray((255 * (noise - np.min(noise)) / (np.max(noise) - np.min(noise))).astype(np.uint8).real)
+                elif noise_type == "pink":
+                    noise = pink_noise(width, height, frequency, attenuation)
+                    noise = Image.fromarray((255 * (noise - np.min(noise)) / (np.max(noise) - np.min(noise))).astype(np.uint8).real)
+                elif noise_type == "green":
+                    noise = green_noise(width, height, frequency, attenuation)
+                    noise = Image.fromarray((255 * (noise - np.min(noise)) / (np.max(noise) - np.min(noise))).astype(np.uint8).real)
+                elif noise_type == "blue":
+                    noise = blue_noise(width, height, frequency, attenuation)
+                    noise = Image.fromarray((255 * (noise - np.min(noise)) / (np.max(noise) - np.min(noise))).astype(np.uint8).real)
+                noise.save(f"noise_{i}.png")
+                
+                blended_image = Image.composite(blended_image, noise, mask)
+                i += 1
+                            
+            return np.asarray(blended_image)
             
         def shorten_to_range(value, min_value, max_value):
             range_length = max_value - min_value + 1
@@ -3800,7 +3819,8 @@ class WAS_Image_Power_Noise:
         if seed is not None:
             if seed > 4294967294:
                 seed = shorten_to_range(seed, 0, 4294967293)
-                cstr(f"Seed too large for power noise; rescaled to: {seed}").warning.print()    
+                cstr(f"Seed too large for power noise; rescaled to: {seed}").warning.print()
+
             np.random.seed(seed)
 
         if noise_type == "white":
@@ -3810,22 +3830,38 @@ class WAS_Image_Power_Noise:
         elif noise_type == "pink":
             if frequency is None:
                 cstr("Pink noise requires a frequency value.").error.print()
+                return None
             noise = pink_noise(width, height, frequency, attenuation)
         elif noise_type == "green":
             if frequency is None:
                 cstr("Green noise requires a frequency value.").error.print()
+                return None
             noise = green_noise(width, height, frequency, attenuation)
         elif noise_type == "blue":
             if frequency is None:
                 cstr("Blue noise requires a frequency value.").error.print()
+                return None
             noise = blue_noise(width, height, frequency, attenuation)
+        elif noise_type == "mix":
+            if frequency is None:
+                cstr("Mix noise requires a frequency value.").error.print()
+                return None
+            if seed is None:
+                cstr("Mix noise requires a seed value.").error.print()
+                return None
+
+            blue_noise_masks = blue_noise_mask(width, height, frequency, attenuation, seed=seed, num_masks=3)
+            noise_types = ["white", "grey", "pink", "green", "blue"]
+            attenuations = [attenuation] * len(noise_types)
+            noise = blend_noise(width, height, blue_noise_masks, noise_types, attenuations)
         else:
             cstr(f"Unsupported noise type `{noise_type}`").error.print()
+            return None
+        if noise_type != 'mix':
+            noise = 255 * (noise - np.min(noise)) / (np.max(noise) - np.min(noise))
+        noise_image = Image.fromarray(noise.astype(np.uint8).real)
 
-        noise = 255 * (noise - np.min(noise)) / (np.max(noise) - np.min(noise))
-        noise_image = Image.fromarray(noise.real.astype(np.uint8))
-
-        return noise_image        
+        return noise_image
 
 # IMAGE TO NOISE
 
