ce0075153503394f3bbd40d92e33142ad7ea8c77
Jordan Thompson <jordanslost@gmail.com>
2023-05-31 23:47:18 -0700

    Update WAS_Node_Suite.py

--- WAS_Node_Suite.py
+++ WAS_Node_Suite.py
@@ -10455,33 +10455,30 @@ class WAS_Samples_Passthrough_Stat_System:
         cstr("\n"+log).msg.print()
             
         return (samples,)
-        
+
     def get_system_stats(self):
         
         import psutil
-        
+    
         # RAM
         ram = psutil.virtual_memory()
         ram_used = ram.used / (1024 ** 3)
         ram_total = ram.total / (1024 ** 3)
         ram_stats = f"Used RAM: {ram_used:.2f} GB / Total RAM: {ram_total:.2f} GB"
-        
+
         # VRAM (with PyTorch)
-        vram_used = torch.cuda.memory_allocated() / (1024 ** 3)
-        vram_total = torch.cuda.max_memory_allocated() / (1024 ** 3)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        vram_used = torch.cuda.memory_allocated(device) / (1024 ** 3)
+        vram_total = torch.cuda.get_device_properties(device).total_memory / (1024 ** 3)
         vram_stats = f"Used VRAM: {vram_used:.2f} GB / Total VRAM: {vram_total:.2f} GB"
-        
-        # GPU Temperature
-        gpu_temperature = torch.cuda.max_memory_allocated()
-        gpu_stats = f"GPU Temperature: {gpu_temperature}"
-        
+
         # Hard Drive Space
         hard_drive = psutil.disk_usage("/")
         used_space = hard_drive.used / (1024 ** 3)
         total_space = hard_drive.total / (1024 ** 3)
         hard_drive_stats = f"Used Space: {used_space:.2f} GB / Total Space: {total_space:.2f} GB"
-        
-        return [ram_stats, vram_stats, gpu_stats, hard_drive_stats]
+
+        return [ram_stats, vram_stats, hard_drive_stats]
 
 
 # NODE MAPPING
