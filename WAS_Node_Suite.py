1b0e16a3062606f664a9e9e61123fb43fb917698
concarne000 <49512595+concarne000@users.noreply.github.com>
2023-12-05 17:17:09 +1100

    Add randomizer to Load Batch Images
    Seems pretty inoffensive to add and is a handy addition

--- WAS_Node_Suite.py
+++ WAS_Node_Suite.py
@@ -5062,7 +5062,7 @@ class WAS_Load_Image_Batch:
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "mode": (["single_image", "incremental_image"],),
+                "mode": (["single_image", "incremental_image", "random"],),
                 "index": ("INT", {"default": 0, "min": 0, "max": 150000, "step": 1}),
                 "label": ("STRING", {"default": 'Batch 001', "multiline": False}),
                 "path": ("STRING", {"default": '', "multiline": False}),
@@ -5093,11 +5093,18 @@ class WAS_Load_Image_Batch:
             if image == None:
                 cstr(f"No valid image was found for the inded `{index}`").error.print()
                 return (None, None)
-        else:
+        elif mode == 'incremental_image':
             image, filename = fl.get_next_image()
             if image == None:
                 cstr(f"No valid image was found for the next ID. Did you remove images from the source directory?").error.print()
                 return (None, None)
+        else:
+            newindex = random.randint(0, len(fl.image_paths))
+            image, filename = fl.get_image_by_id(newindex)
+            if image == None:
+                cstr(f"No valid image was found for the next ID. Did you remove images from the source directory?").error.print()
+                return (None, None)
+            
 
         # Update history
         update_history_images(new_paths)
