9b0df9fab803d47d4647d619881bfff8742a8311
Alex Yoesting <alexyoe@gmail.com>
2024-04-25 13:47:15 -0400

    Add optimize option to Image Save

--- WAS_Node_Suite.py
+++ WAS_Node_Suite.py
@@ -7133,6 +7133,7 @@ class WAS_Image_Save:
                 "filename_number_start": (["false", "true"],),
                 "extension": (['png', 'jpg', 'jpeg', 'gif', 'tiff', 'webp', 'bmp'], ),
                 "quality": ("INT", {"default": 100, "min": 1, "max": 100, "step": 1}),
+                "optimize_image": (["true", "false"],),
                 "lossless_webp": (["false", "true"],),
                 "overwrite_mode": (["false", "prefix_as_filename"],),
                 "show_history": (["false", "true"],),
@@ -7153,7 +7154,7 @@ class WAS_Image_Save:
     CATEGORY = "WAS Suite/IO"
 
     def was_save_images(self, images, output_path='', filename_prefix="ComfyUI", filename_delimiter='_',
-                        extension='png', quality=100, lossless_webp="false", prompt=None, extra_pnginfo=None,
+                        extension='png', quality=100, optimize_image="true", lossless_webp="false", prompt=None, extra_pnginfo=None,
                         overwrite_mode='false', filename_number_padding=4, filename_number_start='false',
                         show_history='false', show_history_by_prefix="true", embed_workflow="true",
                         show_previews="true"):
@@ -7161,6 +7162,7 @@ class WAS_Image_Save:
         delimiter = filename_delimiter
         number_padding = filename_number_padding
         lossless_webp = (lossless_webp == "true")
+        optimize_image = (optimize_image == "true")
 
         # Define token system
         tokens = TextTokens()
@@ -7262,21 +7264,21 @@ class WAS_Image_Save:
                 output_file = os.path.abspath(os.path.join(output_path, file))
                 if extension in ["jpg", "jpeg"]:
                     img.save(output_file,
-                             quality=quality, optimize=True)
+                             quality=quality, optimize=optimize_image)
                 elif extension == 'webp':
                     img.save(output_file,
                              quality=quality, lossless=lossless_webp, exif=exif_data)
                 elif extension == 'png':
                     img.save(output_file,
-                             pnginfo=exif_data, optimize=True)
+                             pnginfo=exif_data, optimize=optimize_image)
                 elif extension == 'bmp':
                     img.save(output_file)
                 elif extension == 'tiff':
                     img.save(output_file,
-                             quality=quality, optimize=True)
+                             quality=quality, optimize=optimize_image)
                 else:
                     img.save(output_file,
-                             pnginfo=exif_data, optimize=True)
+                             pnginfo=exif_data, optimize=optimize_image)
 
                 cstr(f"Image file saved to: {output_file}").msg.print()
 
