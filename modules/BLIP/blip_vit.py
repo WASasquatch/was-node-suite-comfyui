0c405ac983aed46ff9c770acdfb5a015d8bb6e13
大叶子 <1372755472@qq.com>
2025-04-14 03:49:14 +0000

    fix warning about timm

--- modules/BLIP/blip_vit.py
+++ modules/BLIP/blip_vit.py
@@ -15,7 +15,7 @@ from functools import partial
 
 from timm.models.vision_transformer import _cfg, PatchEmbed
 from timm.models.registry import register_model
-from timm.models.layers import trunc_normal_, DropPath
+from timm.layers import trunc_normal_, DropPath
 from timm.models.helpers import named_apply, adapt_input_conv
 
 from fairscale.nn.checkpoint.checkpoint_activations import checkpoint_wrapper
